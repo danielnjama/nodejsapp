8026f81cebe8f4af7356a199f1b9d40bc9a4e0fd
danielnjama <danielnjama2015@gmail.com>
2024-11-09 23:44:03 +0300

    db configuration

--- db-config.js
+++ db-config.js
@@ -1,13 +1,61 @@
-const mysql = require('mysql2');
+const { Client } = require('pg');
 require('dotenv').config();
 
 const getConnection = () => {
-  return mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'myuser',
-    password: process.env.DB_PASSWORD || 'mypassword',
-    database: process.env.DB_DATABASE || 'mydatabase'
+  return new Client({
+    connectionString: process.env.DATABASE_URL,  // Use connectionString with the URL
+    ssl: {
+      rejectUnauthorized: false // Necessary for some cloud providers (e.g., Heroku)
+    }
   });
 };
 
 module.exports = getConnection;
+
+
+
+
+
+// POSTGRESQL DB CONFIG START
+
+// const { Client } = require('pg');
+// require('dotenv').config();
+
+// const getConnection = () => {
+//   return new Client({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'myuser',
+//     password: process.env.DB_PASSWORD || 'mypassword',
+//     database: process.env.DB_DATABASE || 'mydatabase',
+//     port: process.env.DB_PORT || 5432, // Default PostgreSQL port
+//   });
+// };
+
+// module.exports = getConnection;
+
+//POSTGRESQL DB CONFIG END
+
+
+
+
+
+
+
+
+// MYSQL DB CONFIG START
+
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const getConnection = () => {
+//   return mysql.createConnection({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'myuser',
+//     password: process.env.DB_PASSWORD || 'mypassword',
+//     database: process.env.DB_DATABASE || 'mydatabase'
+//   });
+// };
+
+// module.exports = getConnection;
+
+// MYSQL DB CONFIG END
